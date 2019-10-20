89ece2811459c7bb9458ae512ecbb5653edefa10
jerrylin1121 <jerrylin1121@gmail.com>
2019-10-21 01:15:49 +0800

    add navbar animation and add publications

---
+++ js/main.js
@@ -0,0 +1,31 @@
+// smooth scroll
+$('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#0"]')
+  .click(function(event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+      && 
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      if(target.length==0) {
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: 0
+        }, 1000);
+        return false;
+      }
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+      }
+    }
+  });
