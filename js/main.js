3f71f0627605eeec26220d785ce5098e0f2bc49a
jerrylin1121 <jerrylin1121@gmail.com>
2019-10-25 13:31:09 +0800

    fixed js bug

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 // smooth scroll
 $('a[href*="#"]')
   // Remove links that don't actually link to anything
+  .not('[href="#"]')
   .not('[href="#0"]')
   .click(function(event) {
     // On-page links
